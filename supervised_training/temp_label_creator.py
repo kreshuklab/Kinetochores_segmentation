ce4855829eeab6721f3d5341784a0cf442a69681
Ashwin <capnoi8@gmail.com>
2020-07-02 18:25:29 +0200

    temporary routine to create target volumes

---
+++ supervised_training/temp_label_creator.py
@@ -0,0 +1,20 @@
+import h5py
+import glob
+import pickle
+import numpy as np
+
+flist = glob.glob('/home/samudre/embldata/EGFP/labeldir/*')
+
+for each in flist:
+    target = np.zeros((1, 48, 128, 128))
+    
+    with open(each, 'rb') as new_pkl:
+        labels = pickle.load(new_pkl)
+    
+    for k, v in labels.items():
+        a, b, c = int(v[0] - 426), int(v[1] - 426), int(v[2] - 12)
+        target[0][c, b, a] = 1.0
+        
+    tar_h5_name = each.split('/')[-1].split('.')[0] + '_tar.h5'
+    target_file = h5py.File(tar_h5_name, 'w')
+    target_file.create_dataset('labels', data=target)
