b3f5b56226006025ca7c3cb30a4e876a28d21a43
Ashwin <capnoi8@gmail.com>
2020-10-16 20:08:01 +0200

    minor script modifications

--- supervised_training/labels_dilation/bin_dilation.py
+++ supervised_training/labels_dilation/bin_dilation.py
@@ -2,18 +2,37 @@ import h5py
 import numpy as np
 import torch
 from scipy import ndimage
+import argparse
 import glob
 
 #files = glob.glob('/home/samudre/embldata/EGFP/processed_data/val/*')
-files = glob.glob('/home/samudre/embldata/EGFP/new_processed/data/*')
+#files = glob.glob('/home/samudre/embldata/EGFP/new_processed/data/*')
 
-for each in files:
-	h5file = h5py.File(each, 'r')
-	label = h5file['label']
-	raw = torch.tensor(h5file['raw'])
-	label_mod = torch.tensor(ndimage.binary_dilation(label, iterations=1).astype(label.dtype))
-	fname = each.split('/')[-1]
-	newh5 = h5py.File(fname, 'w')
-	newh5.create_dataset('raw', data=raw)
-	newh5.create_dataset('label', data=label_mod)
-	newh5.close()
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir', default='', help='path to directory with all hdf5 files')
+parser.add_argument('--iter', default=1, help='iterations for dilation of labels')
+
+def bin_dilation(datadir, iterations):
+	"""
+	datadir: directory with all data HDF5 files
+	iterations: label dilation parameter
+	"""
+	files = glob.glob(datadir + '/*')
+
+	for each in files:
+		h5file = h5py.File(each, 'r')
+		label = h5file['label']
+		raw = torch.tensor(h5file['raw'])
+		label_mod = torch.tensor(ndimage.binary_dilation(label, iterations=iterations).astype(label.dtype))
+		fname = each.split('/')[-1]
+		newh5 = h5py.File(fname, 'w')
+		newh5.create_dataset('raw', data=raw)
+		newh5.create_dataset('label', data=label_mod)
+		newh5.close()
+
+if __name__ == '__main__':
+	args = parser.parse_args()
+	datadir = args.dir
+	iterations = args.iter
+
+	bin_dilation(datadir, iterations)
--- supervised_training/scripts/hdf5_to_tiff.py
+++ supervised_training/scripts/hdf5_to_tiff.py
@@ -2,14 +2,18 @@ import h5py
 import tifffile
 import numpy as np
 import argparse
+import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dir', default='', help='path to directory with all hdf5 files')
 
 def hdf5_to_tiff(hdf5_dir):
-	for each in hdf5_dir:
-		tiff_name = each[:-3] + '.tif'
+	hdf5list = glob.glob(hdf5_dir + '*')
+	#print(hdf5list)
+	for each in hdf5list:
+		#tiff_name = each[:-3] + '.tif'
 		file = h5py.File(each, 'r') # read the file
+		tiff_name = 'ds' + each.split('/')[-1].split('.')[0].split('_')[1] + '.tif'
 
 		# providing (48,128,128) for the tif as required by stardist
 		data = file['label'][0] # modify this as per the dataset name in the hdf5 file
